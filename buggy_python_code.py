f8c6715b06e0f314279baea245b20580d4eeabcb
unknown <xmedved5@ad.fi.muni.cz>
2025-04-30 15:14:54 +0200

    idk what to name this one

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,3 +1,5 @@
+import sys 
+import os
 import yaml
 import flask
 
@@ -26,9 +28,9 @@ def fetch_website(urllib_version, url):
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
  
-    try:
+    try: 
         http = urllib.PoolManager()
-        http.request('GET', url)
+        r = http.request('GET', url)
     except:
         print('Exception')
 
@@ -43,7 +45,6 @@ def authenticate(password):
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
 
-
 if __name__ == '__main__':
     print("Vulnerabilities:")
     print("1. Format string vulnerability:")
@@ -51,8 +52,8 @@ if __name__ == '__main__':
     print("3. Yaml deserialization vulnerability:")
     print("4. Use of assert statements vulnerability:")
     choice  = input("Select vulnerability: ")
-    if choice == "1":
-        new_person = Person("Vickie")
+    if choice == "1": 
+        new_person = Person("Vickie")  
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
         urlib_version = input("Choose version of urllib: ")
@@ -63,3 +64,4 @@ if __name__ == '__main__':
     elif choice == "4":
         password = input("Enter master password: ")
         authenticate(password)
+
